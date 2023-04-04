0f113d34367f9aff04d116e6821d617772a027fb
Andreia Dias <a._dias@hotmail.com>
2023-04-04 13:59:34 +0100

    Mudanças que acredito deixarem os meus mentores orgulhosos :P

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+document.querySelector(".board").style.display = "none";
+document.querySelector(".resultado").style.display = "none";
+document.querySelector(".totalres").style.display = "none";
+
 class Player {
-  constructor (name,symbol) {
+  constructor (name,symbol,result) {
       this.name = name;
       this.symbol = symbol;
+      this.result = result;
   }
 }
 
@@ -13,31 +18,38 @@ class Tabuleiro {
   }
 }
 
-const player1 = new Player("","X");
-const player2 = new Player("","O");
+const player1 = new Player("","X",0);
+const player2 = new Player("","O",0);
 let actualplayer = player1;
 let turno= 1;
 let tabuleiro= null;
-let poswin = [];
 
 function addNewPlayers() {
   player1.name = document.querySelector('input[name="player1"]').value;
   player2.name = document.querySelector('input[name="player2"]').value;
-  document.querySelector("h2").innerHTML= "É a vez do player "+actualplayer.name;
+  document.querySelector("h2").innerHTML= "É a vez do player "+actualplayer.name+ " ["+ actualplayer.symbol +"]";
   document.querySelector("h3").innerHTML= "Turno "+turno;
+  totalResult();
 }
 
-function switchPlayer () {
-  if (actualplayer == player1){
-    actualplayer = player2;
-  } else {
-    actualplayer = player1;
-  }
-  turno += 1;
+function totalResult() {
+  document.querySelector(".totalres").style.display = "block";
+  let html="";
+  html+=` 
+  <p><span style="color: rgb(0, 120, 180); font-weight: bold">Resultados:</span></p>
+  <p>${player1.name} [${player1.symbol}] = ${player1.result}</p>
+  <p>${player2.name} [${player2.symbol}] = ${player2.result}</p>
+  </div>`;
+  document.querySelector(".totalres").innerHTML=html;
+}
+
+function defineTab (tab) {
+  tabuleiro = new Tabuleiro(tab);
 }
 
 function winnermoves(tipotab){
   let linha = [];
+  let poswin = [];
   let diferenca;
 
   for (let i = 0; i < tipotab; i++){
@@ -60,88 +72,54 @@ function winnermoves(tipotab){
   }
   poswin.push(linha);
   linha = [];
-  for (let j = tipotab-1; j < (tipotab*tipotab)-1; j+=tipotab-1) { 
-    linha.push(j);
+  for (let i = tipotab-1; i < (tipotab*tipotab)-1; i+=tipotab-1) { 
+    linha.push(i);
   }
   poswin.push(linha);
   linha = [];
   return poswin;
 }
 
-function iniciar3x3() {
-  tabuleiro= new Tabuleiro(3);
-  winnermoves ();
-  /*parece que ao colocar o winnermoves aqui a função não ficou bem*/
-  let html="";
-  html+=`
-    <table>
-      <tr>
-        <td id="l1c1-3x3"><button onclick="play(this, 0)"></button></td> 
-        <td id="l1c2-3x3"><button onclick="play(this, 1)"></button></td>
-        <td id="l1c3-3x3"><button onclick="play(this, 2)"></button></td>
-      </tr>
-      <tr>
-        <td id="l2c1-3x3"><button onclick="play(this, 3)"></button></td>
-        <td id="l2c2-3x3"><button onclick="play(this, 4)"></button></td> 
-        <td id="l2c3-3x3"><button onclick="play(this, 5)"></button></td>
-      </tr>
-      <tr>
-        <td id="l3c1-3x3"><button onclick="play(this, 6)"></button></td>
-        <td id="l3c2-3x3"><button onclick="play(this, 7)"></button></td>
-        <td id="l3c3-3x3"><button onclick="play(this, 8)"></button></td>
-      </tr>
-    </table>`;
-  document.querySelector(".board").innerHTML=html;
+function switchPlayer () {
+  if (actualplayer == player1){
+    actualplayer = player2;
+  } else {
+    actualplayer = player1;
+  }
+  turno += 1;
 }
 
-function iniciar4x4() {
-  tabuleiro= new Tabuleiro(4);
-  winnermoves ();
-  let html="";
-  html+=` 
-    <table>
-      <tr>
-        <td id="l1c1-4x4"><button onclick="play(this, 0)"></button></td> 
-        <td id="l1c2-4x4"><button onclick="play(this, 1)"></button></td>
-        <td id="l1c3-4x4"><button onclick="play(this, 2)"></button></td>
-        <td id="l1c4-4x4"><button onclick="play(this, 3)"></button></td>
-      </tr>
-      <tr>
-        <td id="l2c1-4x4"><button onclick="play(this, 4)"></button></td>
-        <td id="l2c2-4x4"><button onclick="play(this, 5)"></button></td> 
-        <td id="l2c3-4x4"><button onclick="play(this, 6)"></button></td>
-        <td id="l2c4-4x4"><button onclick="play(this, 7)"></button></td>
-      </tr>
-      <tr>
-        <td id="l3c1-4x4"><button onclick="play(this, 8)"></button></td>
-        <td id="l3c2-4x4"><button onclick="play(this, 9)"></button></td>
-        <td id="l3c3-4x4"><button onclick="play(this, 10)"></button></td>
-        <td id="l3c4-4x4"><button onclick="play(this, 11)"></button></td>
-      </tr>
-      <tr>
-      <td id="l4c1-4x4"><button onclick="play(this, 12)"></button></td>
-      <td id="l4c2-4x4"><button onclick="play(this, 13)"></button></td>
-      <td id="l4c3-4x4"><button onclick="play(this, 14)"></button></td>
-      <td id="l4c4-4x4"><button onclick="play(this, 15)"></button></td>
-    </tr>
-    </table>`;
-  document.querySelector(".board").innerHTML=html;
+function iniciar() {
+  document.querySelector(".intro").style.display = "none";
+  winnermoves();
+  document.querySelector(".board").style.display = "flex";
+  let html = "<table>";
+  for (let i = 0; i < tabuleiro.tipotab; i++) {
+    html += "<tr>";
+    for (let j = 0; j < tabuleiro.tipotab; j++) {
+      html += `<td id="l${i + 1}c${j + 1}-${tabuleiro.tipotab}x${tabuleiro.tipotab}"><button onclick="play(this, ${i * tabuleiro.tipotab + j})"></button></td>`;
+    }
+    html += "</tr>";
+  }
+  html += "</table>";
+  document.querySelector(".board").innerHTML = html;
 }
 
 function play (element, position) {
   if (element.innerText === ""){
     element.innerText = actualplayer.symbol;
     tabuleiro.jogadas[position]= actualplayer.symbol;
-    switchPlayer();
+    tie();
     winGame ();
+    switchPlayer();
   }
-  document.querySelector ("h2").innerHTML = "É a vez do player " + actualplayer.name;
+  document.querySelector ("h2").innerHTML = "É a vez do player "+actualplayer.name+ " ["+ actualplayer.symbol +"]";
   document.querySelector ("h3").innerHTML = "Turno " + turno;
 }
 
 function winGame () {
   let w=0;
-  for (win of poswin) {
+  for (win of tabuleiro.winner) {
     for (i=0; i<win.length; i++) {
       if (tabuleiro.jogadas [win[i]] === actualplayer.symbol){
         w++;
@@ -151,132 +129,46 @@ function winGame () {
       }
     }
     if (w===win.length) {
-      console.log ("WIN")
-    }
-  }
-}
-
-/*Isto não está a funcionar*/
-
-
-
-  /*
-JS FIDDLE WIN:
-
-let a= [[0,1,2], [3,4,5], [6,7,8]];
-let jogadas= ["O", "X", "X", "", "O", "", "X", "X", "X"];
-let actualplayer = "X";
-let w= 0;
-
-for (let win of a) {
-	for (i=0; i<win.length; i++){
-  	if (jogadas[win[i]] == actualplayer) {
-    	w++;
-    }
-    else {
-    	w=0;
-    }
-   	console.log (w);
-  }
-  if (w==win.length) {
-  	console.log ("WIN")
-  }
-  else {
-  	console.log ("TRY AGAIN")
-  }
-}
-
-
-    if (w==win.length) {
+      document.querySelector(".board").style.display = "none";
+      document.querySelector(".players").style.display = "none";
+      document.querySelector(".resultado").style.display="block";
       turno= 0;
       let html="";
       html+=` 
-        <p>Ganhou o jogador <b>${turno}</b></p>
+        <p>Ganhou o jogador <b>${actualplayer.name} [${actualplayer.symbol}]</b></p>
         <div class="chooseAfterGame">
-          <button id="playAgain" type="button" onclick="iniciar3x3()">Play again</button>
+          <button id="playAgain" type="button" onclick="playAgain()">Jogar novamente</button>
           <button id="restart" type="button" onclick=window.location.reload()>Restart</button>
         </div>`;
       document.querySelector(".resultado").innerHTML=html;
       w=0;
+      actualplayer.result+=1;
+      totalResult();
       return
     }
   }
 }
 
-
-/* TPC: Fazer função winGame e descobrir como omitir as divs, eventualmente pensar como podemos fazer o tabuleiro de forma dinamica
-
-Tenho de conseguir colocar na função winGame o tipotab e fazer com que o if contemple o tipo tab... como fazer isso?
-
-- Ter uma função que faça reset ao jogo -> Lembrar que a variável jogo3x3 ou jogo 4x4 não deu reset, mesmo que nós tenhamos dado reset ao board! O tabuleiro só tem
-a parte HTML, mas não tem a estrutura JS que nos diz quem ganhou ou não!
-
-- Aprender a omitir coisas (conseguimos omitir as divs no nosso javascript, por isso não precisamos de as eliminar no html como tinha antes -> https://www.w3schools.com/jsref/prop_style_visibility.asp)
-
-function winner3x3 () {
-  for (win of winnermoves3x3) {
-    if (jogo3x3 [win[0]] === currentPlayer && jogo3x3 [win[1]] === currentPlayer && jogo3x3 [win[2]] === currentPlayer){
-      contagemturno= 0;
-      let html="";
-      html+=` 
-        <p>Ganhou o jogador <b>${turno}</b></p>
-        <div class="chooseAfterGame">
-          <button id="playAgain" type="button" onclick="iniciar3x3()">Play again</button>
-          <button id="restart" type="button" onclick=window.location.reload()>Restart</button>
-        </div>`;
-      document.querySelector(".resultado").innerHTML=html;
-      return
-    }
-    else {
-      tie3x3();
-    }
-  }
-}
-
-function winner4x4 () {
-  for (win of winnermoves4x4) {
-    if (jogo4x4 [win[0]] === currentPlayer && jogo4x4 [win[1]] === currentPlayer && jogo4x4 [win[2]] === currentPlayer && jogo4x4 [win[3]]=== currentPlayer){
-      contagemturno= 0;
-      let html="";
-      html+=` 
-        <p>Ganhou o jogador <b>${turno}</b></p>
-        <div class="chooseAfterGame">
-          <button id="playAgain" type="button" onclick="iniciar4x4()">Play again</button>
-          <button id="restart" type="button" onclick=window.location.reload()>Restart</button>
-        </div>`;
-      document.querySelector(".resultado").innerHTML=html;
-    }
-    else {
-      tie4x4();
-    }
-  }
-}
-
-function tie3x3() {
-  if (contagemturno >= jogo3x3.length) {
-    contagemturno= 0;
+function tie() {
+  if (turno === tabuleiro.jogadas.length) {
+    document.querySelector(".board").style.display = "none";
+    document.querySelector(".players").style.display = "none";
+    document.querySelector(".resultado").style.display="block";
+    turno= 0;
     let html="";
     html+=` 
       <p><b>Empate!</b></p>
       <div class="chooseAfterGame">
-        <button id="playAgain" type="button" onclick="iniciar3x3()">Play again</button>
+        <button id="playAgain" type="button" onclick="playAgain()">Jogar novamente</button>
         <button id="restart" type="button" onclick=window.location.reload()>Restart</button>
       </div>`;
     document.querySelector(".resultado").innerHTML=html;
   }
 }
 
-function tie4x4() {
-  if (contagemturno >= jogo4x4.length) {
-    contagemturno= 0;
-    let html="";
-    html+=` 
-      <p><b>Empate!</b></p>
-      <div class="chooseAfterGame">
-        <button id="playAgain" type="button" onclick="iniciar4x4()">Play again</button>
-        <button id="restart" type="button" onclick=window.location.reload()>Restart</button>
-      </div>`;
-    document.querySelector(".resultado").innerHTML=html;
-  }
+function playAgain (){
+  defineTab (tabuleiro.tipotab);
+  iniciar();
+  document.querySelector(".players").style.display = "block";
+  document.querySelector(".resultado").style.display="none";
 }
-*/
